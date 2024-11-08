b696cf90128c81d85f8167676f5657c90ee47120
Yifan Luo <luoyifan902008@126.com>
2024-11-08 16:44:49 +0800

    更改

--- lyf_demo_python/src/go2_ros2_slam/launch/cloud_to_scan.py
+++ lyf_demo_python/src/go2_ros2_slam/launch/cloud_to_scan.py
@@ -8,7 +8,7 @@ def generate_launch_description():
             executable='pointcloud_to_laserscan_node',
             name='pointcloud_to_laserscan',
             parameters=[{
-                'target_frame': 'utlidar_lidar',
+                'target_frame': 'base_link',
                 'transform_tolerance': 0.1,
                 'min_height': 0.0,
                 'max_height': 1.0,
@@ -21,8 +21,29 @@ def generate_launch_description():
                 'use_inf': True,
             }],
             remappings=[
-                ('cloud_in', '/utlidar/cloud'),
+                # ('cloud_in', '/lio_sam_ros2/deskew/cloud_deskewed'),
+                ('cloud_in', '/rslidar_points'),                
                 ('scan', '/scan'),
             ]
         ),
+
+
+        Node(
+            package='slam_toolbox',
+            executable='async_slam_toolbox_node',
+            name='slam_toolbox',
+            output='screen',
+            parameters=[{
+                'use_sim_time': False,  # 如果在仿真中使用，设置为True
+                'scan_topic': '/scan',  # 设置激光雷达话题
+                'scan_queue_size': 100,  # 设置消息队列大小
+                'odom_frame': 'odom',  # 里程计的参考坐标系
+                'base_frame': 'odom_mapping',  # 里程计数据的子坐标系
+                'odom_topic': '/lio_sam_ros2/mapping/odometry' # 实际的里程计话题名称
+                # 其他slam_toolbox参数
+            }],
+
+        ),
+
+
     ])
