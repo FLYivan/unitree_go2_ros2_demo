602cea7fccabd6cbd327be9c5f9da6199c1ac381
Yifan Luo <luoyifan902008@126.com>
2025-02-10 16:36:16 +0800

    更新demo

--- lyf_demo_python/src/go2_ros2_basic/go2_ros2_basic/sport_demo.py
+++ lyf_demo_python/src/go2_ros2_basic/go2_ros2_basic/sport_demo.py
@@ -1,4 +1,5 @@
 import rclpy  # 导入ROS2 Python客户端库
+import time
 
 from rclpy.node import Node  # 从rclpy中导入Node类
 from unitree_go.msg import SportModeState  # 导入SportModeState消息类型
@@ -20,10 +21,13 @@ class SportDemo(Node):
         self.vy = 0.0                                                                    # 初始化初始y位置
         self.vyaw = 0.0                                                                  # 初始化初始偏航角
 
+        self.relax_flag = True                                                        # 初始化休息标签
+        self.newyear = True 
 
         self.req_puber = self.create_publisher(Request, '/api/sport/request', 10)                               # 创建'/api/sport/request'的发布者
 
-        
+        # 启动
+        self.Start() 
 
 
         self.timer = self.create_timer(self.dt, self.timer_callback)                    # 创建一个定时器，每0.05秒（create_time单位秒）调用一次timer_callback函数
@@ -31,15 +35,28 @@ class SportDemo(Node):
     def timer_callback(self):
         self.t += self.dt                                                               # 等待1秒后启动
         if self.t >= 0:                                                                 # 检查运行时间计数是否为非负
- 
-            # self.VelocityMove()
-            # self.Start()
-            # self.Relax()
-            self.Stop()
-            # 后续在这里继续添加其他运动测试方法
 
-        elif self.t >=30:
+            self.VelocityMove()
+            
+
+        elif self.t >=6:
+            if self.relax_flag :
+                self.Relax()
+            
+            else :
+              self.VelocityMove()
+
+        elif self.t >=15:
+            self.ChangeGait(0)
+            self.VelocityMove()
+
+        elif self.t >=20:
             self.Stop()
+            if self.newyear :
+                self.NewYear()
+            else :
+                self.Stop()
+
 
     def VelocityMove(self):
   
@@ -49,29 +66,37 @@ class SportDemo(Node):
         self.sport_req.Move(self.req, self.vx, self.vy, self.vyaw)                  # 获取与高级运动命令对应的请求消息
         self.req_puber.publish(self.req)                                            # 发布请求消息
 
-
+    # 切换步态 (只要一次，后面还要切回正常步态)
+    def ChangeGait(self,num: int):
+        # 0  为  idle， 1  为  trot，2  为  trot running，3  正向爬楼模式，4：逆向爬楼模式。
+        self.sport_req.SwitchGait(self.req,num)
+        self.req_puber.publish(self.req) 
     
     def Relax(self):
 
-        # # 坐下
-        # self.sport_req.Sit(self.req)
-        # self.req_puber.publish(self.req) 
-
-        # # 站起（相对于坐下）
-        # self.sport_req.RiseSit(self.req)
-        # self.req_puber.publish(self.req) 
-
-
-        # # 伸懒腰Stretch()
-        # self.sport_req.Stretch(self.req)
-        # self.req_puber.publish(self.req) 
-
-        # 切换步态 (只要一次，后面还要切回正常步态)
-        self.sport_req.SwitchGait(self.req,3)
+        # 伸懒腰Stretch()
+        self.sport_req.Stretch(self.req)
         self.req_puber.publish(self.req) 
 
-        # 切换步态后要move
+        # 坐下
+        self.sport_req.Sit(self.req)
+        self.req_puber.publish(self.req) 
 
+        time.sleep(2)
+
+        # 站起（相对于坐下）
+        self.sport_req.RiseSit(self.req)
+        self.req_puber.publish(self.req) 
+
+
+        self.ChangeGait(3)
+        self.relax_flag = False
+
+
+    def NewYear(self):
+        # 拜年(只能一次性)
+        self.sport_req.Scrape(self.req)
+        self.req_puber.publish(self.req) 
 
     def Start(self):
 
@@ -79,16 +104,15 @@ class SportDemo(Node):
         self.sport_req.Hello(self.req)
         self.req_puber.publish(self.req)  
 
-
+        self.newyear = False
 
     def Stop(self):
+        self.sport_req.StopMove(self.req)                                           # 获取与高级运动命令对应的请求消息
+        self.req_puber.publish(self.req)                                            # 发布请求消息
 
-        # self.sport_req.StopMove(self.req)                                           # 获取与高级运动命令对应的请求消息
-        # self.req_puber.publish(self.req)                                            # 发布请求消息
 
-        # 拜年(只能一次性)
-        self.sport_req.Scrape(self.req)
-        self.req_puber.publish(self.req) 
+
+
 
 def main(args=None):
     rclpy.init(args=args)                                                                   # 初始化rclpy
