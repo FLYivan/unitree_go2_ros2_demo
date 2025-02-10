c620bb00e51cff4f9f929a495059bec249f7ee11
lyf_laptop <luoyifan902008@126.com>
2025-02-10 15:49:41 +0800

    运动demo初步测试

--- lyf_demo_python/src/go2_ros2_basic/go2_ros2_basic/sport_demo.py
+++ lyf_demo_python/src/go2_ros2_basic/go2_ros2_basic/sport_demo.py
@@ -10,7 +10,7 @@ class SportDemo(Node):
         super().__init__('sport_demo')
 
         
-        self.dt = 0.05                                                                 # 设置控制时间步长
+        self.dt = 5.0                                                                 # 设置控制时间步长
         
         self.req = Request()                                                            # 初始化Request消息
         self.sport_req = SportClient()                                                  # 实例化一个SportClient对象(这里是json封装的client)
@@ -23,14 +23,19 @@ class SportDemo(Node):
 
         self.req_puber = self.create_publisher(Request, '/api/sport/request', 10)                               # 创建'/api/sport/request'的发布者
 
+        
+
+
         self.timer = self.create_timer(self.dt, self.timer_callback)                    # 创建一个定时器，每0.05秒（create_time单位秒）调用一次timer_callback函数
 
     def timer_callback(self):
         self.t += self.dt                                                               # 等待1秒后启动
         if self.t >= 0:                                                                 # 检查运行时间计数是否为非负
  
-            self.VelocityMove()
-            
+            # self.VelocityMove()
+            # self.Start()
+            # self.Relax()
+            self.Stop()
             # 后续在这里继续添加其他运动测试方法
 
         elif self.t >=30:
@@ -44,45 +49,46 @@ class SportDemo(Node):
         self.sport_req.Move(self.req, self.vx, self.vy, self.vyaw)                  # 获取与高级运动命令对应的请求消息
         self.req_puber.publish(self.req)                                            # 发布请求消息
 
-    # 坐下 Sit()
-    def Sit(self):
-        pass
 
-
-    # 站起（相对于坐下）RiseSit()
-    def Sit(self):
-        pass
-    
-    # 切换步态 
-    def ChangeGait(self):
-        # 取值  0~4，0  为  idle， 1  为  trot，2  为  trot running，3  正向爬楼模式，4：逆向爬楼模式。
-        # SwitchGait(int d)
-        pass
     
     def Relax(self):
 
-        # Sit()
-        # 站起（相对于坐下）RiseSit()
-        # 伸懒腰Stretch()
-        pass
-        # 切换步态 
+        # # 坐下
+        # self.sport_req.Sit(self.req)
+        # self.req_puber.publish(self.req) 
+
+        # # 站起（相对于坐下）
+        # self.sport_req.RiseSit(self.req)
+        # self.req_puber.publish(self.req) 
+
+
+        # # 伸懒腰Stretch()
+        # self.sport_req.Stretch(self.req)
+        # self.req_puber.publish(self.req) 
+
+        # 切换步态 (只要一次，后面还要切回正常步态)
+        self.sport_req.SwitchGait(self.req,3)
+        self.req_puber.publish(self.req) 
+
+        # 切换步态后要move
+
 
     def Start(self):
 
         # 打招呼
-        # Hello()
-        
-        pass
-
+        self.sport_req.Hello(self.req)
+        self.req_puber.publish(self.req)  
 
 
 
     def Stop(self):
 
-        self.sport_req.StopMove(self.req)                                           # 获取与高级运动命令对应的请求消息
-        self.req_puber.publish(self.req)                                            # 发布请求消息
+        # self.sport_req.StopMove(self.req)                                           # 获取与高级运动命令对应的请求消息
+        # self.req_puber.publish(self.req)                                            # 发布请求消息
 
-        # 拜年 Scrape()
+        # 拜年(只能一次性)
+        self.sport_req.Scrape(self.req)
+        self.req_puber.publish(self.req) 
 
 def main(args=None):
     rclpy.init(args=args)                                                                   # 初始化rclpy
