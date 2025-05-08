501e97b246cb0897dcde78af5f2d5d6896e35fa4
Yifan Luo <luoyifan902008@126.com>
2025-05-08 11:04:44 +0800

    添加point-lio节点

---
+++ unitree_go2_ros2_function_pkg/src/slam/point_lio_unilidar/launch/mapping_utlidar.launch.py
@@ -0,0 +1,55 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+    # 获取包路径
+    pkg_point_lio_unilidar = FindPackageShare('point_lio_unilidar')
+    
+    # 创建配置文件路径
+    config_file = PathJoinSubstitution([
+        pkg_point_lio_unilidar,
+        'config',
+        'utlidar.yaml'
+    ])
+
+    # 定义节点
+    transform_node = Node(
+        package='transform_sensors',
+        executable='transform_everything',
+        name='transform_everything',
+        output='screen'
+    )
+
+    mapping_node = Node(
+        package='point_lio_unilidar',
+        executable='pointlio_mapping',
+        name='laserMapping',
+        output='screen',
+        parameters=[
+            config_file,
+            {
+                'use_imu_as_input': False,
+                'prop_at_freq_of_imu': True,
+                'check_satu': True,
+                'init_map_size': 10,
+                'point_filter_num': 1,        # 4, 3
+                'space_down_sample': True,
+                'filter_size_surf': 0.1,      # 0.5, 0.3, 0.2, 0.15, 0.1
+                'filter_size_map': 0.1,       # 0.5, 0.3, 0.15, 0.1
+                'cube_side_length': 1000.0,   # 2000
+                'runtime_pos_log_enable': False
+            }
+        ],
+        remappings=[
+            ('/cloud_registered', '/registered_scan'),
+            ('/aft_mapped_to_init', '/state_estimation')
+        ]
+    )
+
+    # 返回启动描述
+    return LaunchDescription([
+        transform_node,
+        mapping_node
+    ])
