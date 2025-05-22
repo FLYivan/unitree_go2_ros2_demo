872b84652bc5dc6825ed0066f7331efb86c64c11
Yifan Luo <luoyifan902008@126.com>
2025-05-22 11:46:39 +0800

    添加一系列点云降采样节点

---
+++ unitree_go2_ros2_function_pkg/src/pcl_process_py/setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+package_name = 'pcl_process_py'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+         ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    description='LiDAR pcl process use python.',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'lidar_odometry = pcl_process_py.lidar_odometry_pointcloud:main',
+        ],
+    },
+)
