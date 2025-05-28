ea5589e513f3c00ad14b78e2acdca2691be1bf89
go2 pc2 by Lyf <luoyifan902008@126.com>
2025-05-28 11:43:17 +0800

    add pc2 setup file

---
+++ setup_pc2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Setup unitree go2 in ros2 environment on pc2"
+
+source ~/dog_robot/unitree_go2_ros2_demo/cyclonedds_ws/install/local_setup.bash
+source ~/dog_robot/unitree_go2_ros2_demo/explore/m-explore_ws/install/local_setup.bash
+source ~/dog_robot/unitree_go2_ros2_demo/unitree_go2_ros2_function_pkg/install/local_setup.bash
+
+
+
+
