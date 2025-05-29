853f68280c79710a48322251fe5cc14af62bb4d6
FLYivan <luoyifan902008@126.com>
2025-05-29 18:00:44 +0800

    Update setup_laptop.sh

--- setup_laptop.sh
+++ setup_laptop.sh
@@ -3,7 +3,6 @@ echo "Setup unitree go2 in ros2 environment on laptop"
 # source /opt/ros/humble/setup.bash
 # source $HOME/dog_robot/unitree/unitree_ros2/cyclonedds_ws/install/setup.bash
 source $HOME/dog_robot/unitree_go2_ros2_demo/cyclonedds_ws/install/local_setup.bash
-source $HOME/dog_robot/unitree_go2_ros2_demo/explore/m-explore_ws/install/local_setup.bash
 source $HOME/dog_robot/unitree_go2_ros2_demo/unitree_go2_ros2_function_pkg/install/local_setup.bash
 
 
